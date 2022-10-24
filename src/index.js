35828f13ba6f9d66827efd7e1318c51070c45844
Oleksandr_Mochakov <sasha.mochakov@ukr.net>
2022-10-24 14:16:34 +0300

    add class is-hidden

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const getEl = x => document.querySelector(x);
 let numberPage = 1;
 let inputVal;
+let calcCard = 0;
 
 getEl('#search-form').addEventListener('submit', onSubmitForm);
 getEl('.load-more').addEventListener('click', onClick);
@@ -20,6 +21,7 @@ async function onSubmitForm(event) {
     const getData = await axios.get(
       `https://pixabay.com/api/?key=30743658-420b85cc9935254192e6e5527&q=${inputVal}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${numberPage}`
     );
+    const addClsIsHid = await getEl('.load-more').classList.remove('is-hidden');
     const arrObj = await onArrObj(getData);
     const drawCard = await arrObj
       .map(el => {
@@ -44,7 +46,7 @@ async function onSubmitForm(event) {
       })
       .join(' ');
     const addMarkup = await getDataOnRequest();
-    const addClsIsHid = await getEl('.load-more').classList.remove('is-hidden');
+
     function getDataOnRequest() {
       return (getEl('.gallery').innerHTML = drawCard);
     }
@@ -65,6 +67,10 @@ async function onSubmitForm(event) {
     const {
       data: { hits, totalHits },
     } = response;
+    calcCard = hits.length;
+    if (calcCard === totalHits) {
+      getEl('.load-more').classList.add('is-hidden');
+    }
     if (hits.length < 1) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
@@ -113,11 +119,13 @@ async function onClick() {
       const {
         data: { hits, totalHits },
       } = response;
-      if (hits.length < 1) {
-        Notiflix.Notify.warning(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
+      // if (hits.length < 1) {
+      // Notiflix.Notify.warning(
+      //   'Sorry, there are no images matching your search query. Please try again.'
+      // );
+      calcCard += hits.length;
+      if (calcCard === totalHits) {
+        getEl('.load-more').classList.add('is-hidden');
       } else {
         Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
       }
